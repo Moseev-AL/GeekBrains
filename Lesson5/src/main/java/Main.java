367a6691df1ef9721881ca6ce0f30556218494ee
moseev-al <moseev.al@gmail.com>
2020-07-30 20:55:38 +0300

    Java 2. Lesson 5

--- Lesson5/src/main/java/Main.java
+++ Lesson5/src/main/java/Main.java
@@ -1,65 +1,63 @@
 
 public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    static float[] arr = new float[size];
 
-    /**
-     * Пытаемся преобразовать строку в Int
-     * @param text
-     * @return
-     */
-    public static Object tryParse(String text) {
-        try {
-            return Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            return null;
+
+    public static void generateSolidArray() {
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
         }
+        long s = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        long f = System.currentTimeMillis();
+        System.out.println("Solid array processing time: "+ (f - s));
     }
 
-    /**
-     * Проверка массива на превышение длины
-     * @param arr
-     * @return True, если массив содержит элемент некорректной длины
-     */
-    public static boolean checkArrayLength(String[][] arr){
-        int arrayMaxLength = 4;
-        if (arr.length > arrayMaxLength){
-           return false;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length > arrayMaxLength) {
-                return false;
-            }
+    public static void generateSplittedArrays() {
+
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
         }
 
-        return true;
+        long s = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread threadA1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread threadA2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + (i+h) / 5) * Math.cos(0.2f + (i+h) / 5) * Math.cos(0.4f + (i+h) / 2));
+            }
+        });
+
+        threadA1.start();
+        threadA2.start();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        long f = System.currentTimeMillis();
+        System.out.println("Splitted array processing time: "+ (f - s));
     }
 
-    public static void generateArrayException(String[][] arr) {
-        try {
-            if (!checkArrayLength(arr)) {
-                throw new ArraySizeException();
-            }
-
-            int arrSum = 0;
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    Object val = tryParse(arr[i][j]);
-                    if (val == null) {
-                        throw new ArrayDataException(i, j);
-                    }
-                    arrSum += (int) val;
-                }
-            }
-            System.out.println("Сумма элементов = " + arrSum);
-        } catch (ArrayDataException | ArraySizeException ex) {
-            System.out.println(ex.getExceptionMessage());
-        }
-
-    }
 
     public static void main(String[] args) {
-        String[][] arr = new String[][]{{"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}, {"1", "1", "1", "1"}};
-        generateArrayException(arr);
-
+        generateSolidArray();
+        generateSplittedArrays();
     }
 
 }
